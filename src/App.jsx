1528b0161b8d06c0e00dd68f16fbd49d370cd87a
Saurav <138647854+ksaurav24@users.noreply.github.com>
2024-10-11 08:55:13 +0530

    Refactor App.jsx to remove unused imports and clean up code

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import BalanceBike from "./BalanceBike";
 import "./Roboto.css";
 import Navbar from "./Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './App.css'
 import Hero from './Hero'
-import BalanceBike from './BalanceBike'
 
 function App() {
   return (
