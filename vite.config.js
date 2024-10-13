e95b0a2d86538e348857e8dd9d76f834b62127c7
Saurav <138647854+ksaurav24@users.noreply.github.com>
2024-10-13 13:25:34 +0530

    Login and Signup with profile page setup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.glb"], // Add this line to ignore .glb files
+  assetsInclude: ["**/*.glb"],
+  build: {
+    rollupOptions: {
+      input: "index.html", // This is your entry point
+    },
+  },
 });
